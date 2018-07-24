b64127dd1c64257ec1f0f66278bcef916526132f
yanal1997 <einal1997@gmai.com>
2018-07-24 09:56:53 +0300

    SSH connection

--- Ssh/src/main/java/ssh_connection/Ssh/Main.java
+++ Ssh/src/main/java/ssh_connection/Ssh/Main.java
@@ -1,23 +1,28 @@
 package ssh_connection.Ssh;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String host, userName, password, command;
-
+		System.out.println("hi");
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String line = in.readLine();
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("enter host name");
-		host = scanner.next();
+		host = in.readLine();
 		System.out.println("enter user name");
-		userName = scanner.next();
+		userName = in.readLine();
 		System.out.println("enter password");
-		password = scanner.next();
+		password = in.readLine();
 		SshConnection connection = new SshConnection(host, userName, password);
 		connection.connected();
 		System.out.println("enter command:");
-		command = scanner.next();
+		command = in.readLine();
 		connection.setCommand(command);
 		while(!command.equals("close")){
 			connection.command();
